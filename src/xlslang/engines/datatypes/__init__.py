3278f19eee6e602234f4f3f9b50d00b31e8a7c26
Edward Villegas-Pulgarin <cosmoscalibur@gmail.com>
2021-12-05 18:24:31 -0500

    Datatype XString fix object definition

---
+++ src/xlslang/engines/datatypes/__init__.py
@@ -0,0 +1,69 @@
+from decimal import Decimal
+
+class XString:
+
+    def __init__(self, text:str):
+        self.value = text
+        self._number = None
+
+    def __str__(self):
+        return self.value
+
+    @property
+    def number(self):
+        if self._number is None:
+            self._number = Decimal(self.value)
+        return self._number
+
+    def join(self, text_list):
+        joined = self.value.join([text.value for text in text_list])
+        return XString(joined)
+
+    def __neg__(self) -> Decimal:
+        return -self.number
+
+    def __pos__(self) -> Decimal:
+        return self.number
+
+    def __abs__(self) -> Decimal:
+        return abs(self.number)
+
+    def __add__(self, other) -> Decimal:
+        if isinstance(other, XString):
+            other = other.number
+        return self.number + other
+
+    def __radd__(self, other) -> Decimal:
+        return self.__add__(other)
+
+    def __sub__(self, other) -> Decimal:
+        if isinstance(other, XString):
+            other = other.number
+        return self.number - other
+
+    def __rsub__(self, other) -> Decimal:
+        return other + self.__neg__()
+
+    def __mul__(self, other) -> Decimal:
+        if isinstance(other, XString):
+            other = other.number
+        return self.number * other
+
+    def __rmul__(self, other) -> Decimal:
+        return self.__mul__(other)
+
+    def __truediv__(self, other) -> Decimal:
+        if isinstance(other, XString):
+            other = other.number
+        return self.number / other
+
+    def __rtruediv__(self, other) -> Decimal:
+        return self.__truediv__(other)
+
+    def __mod__(self, other) -> Decimal:
+        if isinstance(other, XString):
+            other = other.number
+        return self.number % other
+
+    def __rmod__(self, other) -> Decimal:
+        return self.__mod__(other)
